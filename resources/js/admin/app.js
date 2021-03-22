b14d0f3b4d0c2984799c15a7b7bb95ebdb110ee1
paul <paul.anderson.b.garcia@gmail.com>
2021-03-22 10:01:45 +0800

    test

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -3,7 +3,7 @@ require('../bootstrap');
 window.Vue = require('vue');
 
 import router from './src/router/index'
-import vuetify from './src/plugin/vuetify'
+import vuetify from './src/plugin/vuetify/index'
 
 Vue.component('main-app', require('@admin/layout/App.vue').default);
 
