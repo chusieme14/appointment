99c09afee5544e76fdd63ed7668f27d655617100
paul <paul.anderson.b.garcia@gmail.com>
2021-06-19 11:13:56 +0800

    prob build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        
-    </head>
-    <body>
-        <div id="app">
-            <main-app></main-app>
-        </div>
-        <script src="{{ asset('js/adminjs/app.js')}}"></script>
-    </body>
-</html> --}}
 <!DOCTYPE html>
 <html lang="en">
 <head>
